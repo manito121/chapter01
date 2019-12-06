6a70fa4c774442440a9710bdfd849a9ae2aa46ff
zhaoguangyin <guangyin.zhao@beebank.com>
2019-12-06 14:06:58 +0800

    新增文件

---
+++ chapter2/src/main/java/com/smart/lottery/Demo.java
@@ -0,0 +1,19 @@
+package com.smart.lottery;
+
+/**
+ 备注：红球一共6组，每组从1-33中抽取一个，六个互相不重复。然后蓝球是从1-16中抽取一个数字 .
+
+
+ */
+
+public class Demo {
+
+    private String name;
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
