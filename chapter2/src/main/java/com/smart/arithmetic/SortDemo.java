888ce8c987c6c00e23b2510f3852c151a9ee7b37
zhaoguangyin <guangyin.zhao@beebank.com>
2019-12-19 14:53:57 +0800

    20191219(新增文件)

--- chapter2/src/main/java/com/smart/arithmetic/SortDemo.java
+++ chapter2/src/main/java/com/smart/arithmetic/SortDemo.java
@@ -1,9 +1,11 @@
 package com.smart.arithmetic;
 
+import javax.swing.*;
+
 public class SortDemo {
 
     public static void main(String[] args) {
-        int [] arr = new int[10];
+       /* int [] arr = new int[10];
         arr[0] =  77;
         arr[1] =  99;
         arr[2] =  44;
@@ -14,8 +16,14 @@ public class SortDemo {
         arr[7] =  0;
         arr[8] =  66;
         arr[9] =  33;
+        bubbleSort(arr);*/
+        //0000000200122568
+        String result = null;
+        String s1 = "10033658908";
+        Long result1 = Long.valueOf(s1);
+        result=String.format("%0"+16+"d",result1);
+        System.out.println(result);
 
-        bubbleSort(arr);
     }
 
     /**
--- chapter2/src/main/java/com/smart/lottery/Demo.java
+++ chapter2/src/main/java/com/smart/lottery/Demo.java
@@ -11,6 +11,30 @@ public class Demo {
     private String name;
 
     public static void main(String[] args) {
+        String CustNum1 = null;
+        String str = "<ResponseBody><CerType>1</CerType><CerNo>372801196303110032</CerNo><CustName13>蜜银北京j</CustName13><CustNum1>200122568</CustNum1></ResponseBody>";
+        if (str.indexOf("<CustNum1>") != -1) {
+
+            System.out.println(str.length());
+            System.out.println(str.indexOf("<CustNum1>"));
+            System.out.println(str.indexOf("<CustNum1>")+11);
+            CustNum1 = str
+                    .substring(str.indexOf("<CustNum1>"),
+                            str.indexOf("</CustNum1>") + 11);
+        }
+//        System.out.println(CustNum1);
+        
+        if (str.indexOf("<CustNum1>") != -1) {
+
+            System.out.println(str.length());  
+//            System.out.println(str.indexOf("<CustNum1>"));
+//            System.out.println(str.indexOf("<CustNum1>")+11);
+             CustNum1 = str.substring(
+                     str.indexOf("<CustNum1>") + 10,
+                     str.indexOf("</CustNum1>")
+             );
+              System.out.println(CustNum1); 
+        }
 
     }
 
